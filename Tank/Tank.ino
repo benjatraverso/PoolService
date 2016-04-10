b2537935dd166f6f81ffdb90d322d5e0a6f19d5f
benjatraverso <benjatraverso@gmail.com>
2016-04-10 17:47:57 -0300

    mejoras

--- Tank/Tank.ino
+++ Tank/Tank.ino
@@ -1,16 +1,16 @@
-const byte EnableLeft = 10;  // Enable Pin for left motor
-const byte EnableRight = 11;  // Enable Pin for right motor
+const byte EnableLeft = 11;  // Enable Pin for left motor
+const byte EnableRight = 10;  // Enable Pin for right motor
 
 const byte MotorLF = 8;   // Control pin Forward for left motor
-const byte MotorLB = 9;   // Control pin Backward for left motor
+const byte MotorLB = 7;   // Control pin Backward for left motor
 const byte MotorRF = 12;  // Control pin Forward for right motor
 const byte MotorRB = 13;  // Control pin Backward for right motor
 
 const bool FORWARD    = 1;
 const bool BACKWARDS  = 0;
-const int  NORMAL_SPEED = 255/2;
-const int  LOW_SPEED = 255/4;
-const int  HIGH_SPEED = 255;
+const int  NORMAL_SPEED = 100;
+const int  LOW_SPEED = 75;
+const int  HIGH_SPEED = 125;
 
 enum States
 {
@@ -23,6 +23,68 @@ enum States
 
 int state;
 int Speed;
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+  state = eIdle;
+  Serial.begin(9600);
+  pinMode(EnableLeft, OUTPUT); //set enable motor pin as output
+  pinMode(EnableRight, OUTPUT);//same for right one
+
+  //set al motor connected pins as output
+  pinMode(MotorLF, OUTPUT);
+  pinMode(MotorLB, OUTPUT);
+  pinMode(MotorRF, OUTPUT);
+  pinMode(MotorRB, OUTPUT);
+
+  Speed = NORMAL_SPEED;
+}
+
+// the loop function runs over and over again until power down or reset
+void loop()
+{
+  Serial.println("State: ");
+  Serial.println(state);
+  switch (state)
+  {
+    case eIdle:
+    {
+      eStateBeIdle();
+      state = eMoveForward;
+      break;
+    }
+    case eMoveForward:
+    {
+      eStateMoveForward(Speed);
+      state = eMoveBackwards;
+      break;
+    }
+    case eMoveBackwards:
+    {
+      eStateMoveBackwards(Speed);
+      state = eTurnLeft;
+      break;
+    }
+    case eTurnLeft:
+    {
+      eStateTurnLeft(Speed);
+      state = eTurnRight;
+      break;
+    }
+    case eTurnRight:
+    {
+      eStateTurnRight(Speed);
+      state = eIdle;
+      break;
+    }
+    default:
+    {
+      state = eIdle;
+    }
+  }
+ delay(5000);
+}
 
 void eStateBeIdle()
 {
@@ -94,64 +156,3 @@ void eStateOneLeft(int Speed)
   setLeft(FORWARD, Speed);
 }
 
-// the setup function runs once when you press reset or power the board
-void setup()
-{
-  state = eIdle;
-  Serial.begin(9600);
-  pinMode(EnableLeft, OUTPUT); //set enable motor pin as output
-  pinMode(EnableRight, OUTPUT);//same for right one
-
-  //set al motor connected pins as output
-  pinMode(MotorLF, OUTPUT);
-  pinMode(MotorLB, OUTPUT);
-  pinMode(MotorRF, OUTPUT);
-  pinMode(MotorRB, OUTPUT);
-
-  Speed = NORMAL_SPEED;
-}
-
-// the loop function runs over and over again until power down or reset
-void loop()
-{
-  Serial.println("State: ");
-  Serial.println(state);
-  switch (state)
-  {
-    case eIdle:
-    {
-      eStateBeIdle();
-      state = eMoveForward;
-      break;
-    }
-    case eMoveForward:
-    {
-      eStateMoveForward(Speed);
-      state = eMoveBackwards;
-      break;
-    }
-    case eMoveBackwards:
-    {
-      eStateMoveBackwards(Speed);
-      state = eTurnLeft;
-      break;
-    }
-    case eTurnLeft:
-    {
-      eStateTurnLeft(Speed);
-      state = eTurnRight;
-      break;
-    }
-    case eTurnRight:
-    {
-      eStateTurnRight(Speed);
-      state = eIdle;
-      break;
-    }
-    default:
-    {
-      state = eIdle;
-    }
-  }
- delay(5000);
-}
