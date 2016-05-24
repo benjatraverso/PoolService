f021fa57c3d6a151f2b0794848a8e968cdd124bc
benjatraverso <benjatraverso@gmail.com>
2016-05-24 11:47:03 -0300

    cambios

--- Software/Boat/Boat_V11/Boat_V11.ino
+++ Software/Boat/Boat_V11/Boat_V11.ino
@@ -1,8 +1,8 @@
 //----------------------------------------------------------------------------
+//
 // Versión 1.1
 // Sensores con interrupciones
-// sin pwm para habilitar motores para porder agregar líneas de debug en
-// un arrchivo en tarjeta SD
+//
 //----------------------------------------------------------------------------
 
 #include "Boat.h"
@@ -190,84 +190,6 @@ int readRight( void )
   //leave emitter off for lowering consumtion
   digitalWrite( rightSensorEmitter, LOW );
   return lecture;
-}
-
-//----------------------------------------------------------------------------
-//                                MOVEMENTS
-//----------------------------------------------------------------------------
-void beIdle( void )
-{
-  //can not have delays for many steps may use it to stop motors first
-  killRight();
-  killLeft();
-}
-
-void moveForward( void )
-{
-  // always go to idle first to avoid enabling both pins of motor
-  setRight(FORWARD);
-  setLeft(FORWARD);
-}
-
-void moveBackwards( void )
-{
-  // always go to idle first to avoid enabling both pins of motor
-  setRight(BACKWARDS);
-  setLeft(BACKWARDS);
-}
-
-void turnFullLeft( void )
-{
-  setRight(BACKWARDS);
-  setLeft(FORWARD);
-}
-
-void turnFullRight( void )
-{
-  setRight(FORWARD);
-  setLeft(BACKWARDS);
-}
-
-void turnRight( void )
-{
-  killLeft();
-  setRight(FORWARD);
-}
- 
-void turnLeft( void )
-{
-  killRight();
-  setLeft(FORWARD);
-}
-
-void setRight( bool Direction )
-{
-  killRight();
-  digitalWrite( MotorRF, Direction );
-  digitalWrite( MotorRB, !Direction );
-  analogWrite( EnableRight, glSpeed );
-}
-
-void setLeft( bool Direction )
-{
-  killLeft();
-  digitalWrite( MotorLF, Direction );
-  digitalWrite( MotorLB, !Direction );
-  analogWrite( EnableLeft, glSpeed );
-}
-
-void killRight( void )
-{
-  digitalWrite( MotorRF, Direction );
-  digitalWrite( MotorRB, !Direction );
-  digitalWrite( EnableRight, LOW );
-}
-
-void killLeft( void )
-{
-  digitalWrite( MotorLF, LOW );
-  digitalWrite( MotorLB, LOW );
-  digitalWrite( EnableLeft, LOW );
 }
 
 void inError( void )
