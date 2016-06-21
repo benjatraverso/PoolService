8c365763aede015ae42881e941d723b5350fe7b1
benjatraverso <benjatraverso@gmail.com>
2016-06-21 14:04:22 -0300

    avanzando con la versión con pooling

---
+++ Software/Boat/Boat_V09/Boat.h
@@ -0,0 +1,84 @@
+#ifndef BOAT_LIB
+#define BOAT_LIB
+
+//----------------------------------------------------------------------------
+//                            CONSTANTS
+//----------------------------------------------------------------------------
+const byte EnableLeft = 11;   // Enable Pin for left motor
+const byte EnableRight = 10;  // Enable Pin for right motor
+
+const byte MotorLF = 8;   // Control pin Forward for left motor
+const byte MotorLB = 7;   // Control pin Backward for left motor
+const byte MotorRF = 12;  // Control pin Forward for right motor
+const byte MotorRB = 13;  // Control pin Backward for right motor
+
+const bool FORWARD              = 1;
+const bool BACKWARDS            = 0;
+const bool RIGHT                = 1;
+const bool LEFT                 = 0;
+
+const byte leftProximitySensor  = 2;   //interrupt input 0 reads left sensor (DP2 is interrupt 0)
+const byte rightProximitySensor = 3;   //interrupt input 1 reads right sensor (DP3 is interrupt 1)
+const byte leftSensor    = 5;
+const byte rightSensor   = 4;
+
+const byte NORMAL_SPEED         = 100;
+const byte LOW_SPEED            = 75;
+const byte HIGH_SPEED           = 125;
+const int SECONDS               = 1000;
+const int STEPS_DELAY           = 3 * SECONDS;
+const int FULL_TURN_TIME      	= 10 * SECONDS;		// 10 seconds to pass from turning to 2 motors turnning
+const int ERROR_TIME		        = 30 * SECONDS;   // seconds to determin we are stuck
+const int ERROR_DELAY           = 10 * SECONDS;
+const int POOLING_DELAY         = 1 * SECONDS;
+
+const int NEITHER     = -1;
+const int TURN_LEFT   = 0;
+const int TURN_RIGHT  = 1;
+
+//----------------------------------------------------------------------------
+//                            ENUMS
+//----------------------------------------------------------------------------
+enum States
+{
+  eIdle = 0,
+  eMoveForward,
+  eMoveBackwards,
+  eTurn,
+  eTurnFull,
+  eError
+};
+
+#endif
+
+//                               +-----___-----+
+//                               |             |
+//                         RESET | 1        28 | AI5
+//                               |             |
+//                           RXD | 2        27 | AI4
+//                               |             |
+//                           TXD | 3        26 | AI3
+//                               |             |
+// leftProximitySensor  INT0 PD2 | 4        25 | AI2
+//                               |             |
+// rightProximitySensor INT1 PD3 | 5        24 | AI1
+//                               |             |
+// rightSensor               PD4 | 6        23 | AI0
+//                               |             |
+//                           VCC | 7        22 | GND
+//                               |             |
+//                           GND | 8        21 | AREF
+//                               |             |
+//                         XTAL1 | 9        20 | AVCC
+//                               |             |
+//                         XTAL2 | 10       19 | DP13         MotorRB
+//                               |             |
+// leftSensor          (PWM) DP5 | 11       18 | DP12         MotorRF
+//                               |             |
+//                     (PWM) DP6 | 12       17 | DP11 (PWM)   EnableLeft
+//                               |             |
+// MotorLB                   DP7 | 13       16 | DP10 (PWM)   EnableRight
+//                               |             |
+// MotorLF                   DP8 | 14       15 | DP9 (PWM)
+//                               |             |
+//                               +-------------+
---
+++ Software/Boat/Boat_V09/Boat_V112.ino
@@ -0,0 +1,387 @@
+//----------------------------------------------------------------------------
+//
+// Versión 1.1.2
+// Interrupt throw sensors with pooling
+//
+//----------------------------------------------------------------------------
+
+#include "Boat.h"
+//----------------------------------------------------------------------------
+//                            GLOBAL VARIABLES
+//----------------------------------------------------------------------------
+byte glState; // TODO: guess there is no need for this to be global
+byte glDirection = NEITHER;
+int glSpeed = NORMAL_SPEED;// TODO: change to HIGH_SPEED for solar version
+//----------------------------------------------------------------------------
+//                            SETUP
+//----------------------------------------------------------------------------
+void setup()
+{
+  Serial.begin( 9600 );//for testing purposes TODO: remove serial comm for release version
+  pinMode( EnableLeft, OUTPUT ); //set enable motor pin as output
+  pinMode( EnableRight, OUTPUT );//same for right one
+  
+  pinMode( leftSensor, OUTPUT );
+  pinMode( rightSensor, OUTPUT );
+
+  pinMode( rightProximitySensor, INPUT );
+  pinMode( leftProximitySensor, INPUT );
+
+  //set al motor connected pins as output
+  pinMode( MotorLF, OUTPUT );
+  pinMode( MotorLB, OUTPUT );
+  pinMode( MotorRF, OUTPUT );
+  pinMode( MotorRB, OUTPUT );
+
+  // start with everything dead
+  glState = eIdle;
+}
+
+//----------------------------------------------------------------------------
+//                            LOOP
+//----------------------------------------------------------------------------
+void loop( void )
+{
+  Serial.println("Estate: ");
+  switch ( glState )
+  {
+    case eIdle:
+    {
+      Serial.println("idle");
+      beIdle();
+      glDirection = NEITHER;
+      glState = eMoveForward;
+      break;
+    }
+
+    case eMoveForward:
+    {
+      Serial.print("moving forward ");
+      moveForward();
+
+      // for moving forward we need to open our eyes
+      enableSensors();
+      delay( 500 );
+      
+      glDirection = NEITHER;
+
+      while( NEITHER == glDirection )
+      {
+        if( !digitalRead( leftProximitySensor ) )
+        {
+          Serial.println("right");
+          glDirection = TURN_RIGHT;
+        }
+        
+        if( !digitalRead( rightProximitySensor ) )
+        {
+          Serial.println("left");
+          glDirection = TURN_LEFT;
+        }
+        //TODO: get outta here if've been for too long
+        //also do other stuff while moving forward (future)
+        delay( POOLING_DELAY );
+      }
+      Serial.println("object detected");
+      glState = eTurn;
+      break;
+    }
+
+    case eTurn:
+    {
+      Serial.print("turning ");
+      // record the much time we spend here...
+      unsigned long timePassed = millis();
+    
+      if( TURN_LEFT == glDirection )
+      {
+        Serial.println("left");
+        killLeftSensor();
+        // do the actual turn's task, turn
+        turnLeft();
+      }
+      else
+      {
+        Serial.println("right");
+        killRightSensor();
+        // do the actual turn's task, turn
+        turnRight();
+      }
+      
+      while( NEITHER != glDirection )
+      {
+        // stay here as long as the object is still detected
+        if( FULL_TURN_TIME < ( millis() - timePassed ) )
+        {
+          // or too long to be turning with just on motor
+          glState = eTurnFull;
+          break;//get out of this loop
+        }
+        
+        // only read the sensor we care about
+        // if that input is now high, go to forward
+        if( 
+            ( glDirection == TURN_LEFT && digitalRead( rightProximitySensor ) ) ||
+            ( glDirection == TURN_RIGHT && digitalRead( leftProximitySensor ) )
+          )
+        {
+          glDirection = NEITHER;
+          Serial.println("object gone");
+          glState = eMoveForward;
+        }
+        
+        delay( POOLING_DELAY );
+      }
+
+      break;
+    }
+    
+    case eTurnFull:
+    {
+      Serial.println("turning full");
+      unsigned long timePassed = millis();
+
+      // do the step task
+      int lint = digitalRead( leftProximitySensor );
+      int rint = digitalRead( rightProximitySensor );
+      turnFull( glDirection );
+      
+      // stay here untill there is no more object detected or time's been too long
+      while( NEITHER != glDirection && eTurnFull == glState )
+      {
+        // or we figured there must be an error
+        if( ERROR_TIME < ( millis() - timePassed ) )
+        {
+          glState = eError;
+        }
+        
+        // only read the sensor we care about
+        // if that input is now high, go to forward
+        if( 
+            ( glDirection == TURN_LEFT && digitalRead( rightProximitySensor ) ) ||
+            ( glDirection == TURN_RIGHT && digitalRead( leftProximitySensor ) )
+          )
+        {
+          Serial.println("object gone");
+          glDirection = NEITHER;
+          glState = eMoveForward;
+        }
+        
+        delay( POOLING_DELAY );
+      }
+      
+      break;
+    }
+
+    case eError:
+    {
+      inError();
+      Serial.println("running error state");
+      // no often, only try this step in case of error
+      // all logic is inside the step function.
+      // then start over from idle for we don't know where we are
+      glState = eIdle;
+      break;
+    }
+
+    default:
+    {
+      //we should never be here, but..., just in case
+      glState = eIdle;
+      glDirection = NEITHER;
+    }
+  }
+  delay( STEPS_DELAY );
+}
+
+//----------------------------------------------------------------------------
+//                            SENSORS - INTERRUPTS
+//----------------------------------------------------------------------------
+void rightSensorAlert( void )
+{
+  rint = digitalRead( rightProximitySensor );
+  if( rint )
+  {
+    moveForward();
+  }
+  else
+  {
+    turnLeft();
+  }
+  lint = !rint;
+}
+
+void leftSensorAlert( void )
+{
+  lint = digitalRead( leftProximitySensor );
+  if( lint )
+  {
+    moveForward();
+  }
+  else
+  {
+    turnRight();
+  }
+  lint = !rint;
+}
+
+void enableSensors( void )
+{
+  digitalWrite( leftSensor, HIGH );
+  digitalWrite( rightSensor, HIGH );
+}
+
+void killRightSensor( void )
+{
+  Serial.println("kill right");
+  digitalWrite( rightSensor, LOW );
+}
+
+void killLeftSensor( void )
+{
+  Serial.println("kill left");
+  digitalWrite( leftSensor, LOW );
+}
+
+void detachInterrupts( void )
+{
+  detachInterrupt( digitalPinToInterrupt( rightProximitySensor ) );
+  detachInterrupt( digitalPinToInterrupt( leftProximitySensor ) );
+}
+
+//----------------------------------------------------------------------------
+//                                MOVEMENTS
+//----------------------------------------------------------------------------
+void inError( void )
+{
+  //full speed for all this attempts
+  glSpeed = HIGH_SPEED;
+  //be idle and move back
+  moveBackwards();
+  //keep pushing back for a while
+  delay( ERROR_DELAY );
+  //shake a little
+  turnFullLeft();
+  delay( ERROR_DELAY );
+  turnFullRight();
+  delay( ERROR_DELAY );
+  turnFullLeft();
+  delay( ERROR_DELAY );
+  turnFullRight();
+  delay( ERROR_DELAY );
+  moveBackwards();
+  //end, this will put me on idle later, let's hope the mad dance made it
+  //get stuff back to normal
+  glSpeed = NORMAL_SPEED;
+}
+
+void beIdle( void )
+{
+  //can not have delays for many steps may use it to stop motors first
+  killRight();
+  killLeft();
+}
+
+void moveForward( void )
+{
+  // always go to idle first to avoid enabling both pins of motor
+  setRight( FORWARD );
+  setLeft( FORWARD );
+}
+
+void moveBackwards( void )
+{
+  // always go to idle first to avoid enabling both pins of motor
+  setRight( BACKWARDS );
+  setLeft( BACKWARDS );
+}
+
+void turnFullLeft( void )
+{
+  setRight( BACKWARDS );
+  setLeft( FORWARD );
+}
+
+void turnFullRight( void )
+{
+  setRight( FORWARD );
+  setLeft( BACKWARDS );
+}
+
+void turnRight( void )
+{
+  killLeft();
+  setRight( FORWARD );
+}
+ 
+void turnLeft( void )
+{
+  killRight();
+  setLeft( FORWARD );
+}
+
+void setRight( bool Direction )
+{
+  killRight();
+  digitalWrite( MotorRF, Direction );
+  digitalWrite( MotorRB, !Direction );
+  analogWrite( EnableRight, glSpeed );
+}
+
+void setLeft( bool Direction )
+{
+  killLeft();
+  digitalWrite( MotorLF, Direction );
+  digitalWrite( MotorLB, !Direction );
+  analogWrite( EnableLeft, glSpeed );
+}
+
+void killRight( void )
+{
+  digitalWrite( MotorRF, LOW );
+  digitalWrite( MotorRB, LOW );
+  digitalWrite( EnableRight, LOW );
+}
+
+void killLeft( void )
+{
+  digitalWrite( MotorLF, LOW );
+  digitalWrite( MotorLB, LOW );
+  digitalWrite( EnableLeft, LOW );
+}
+
+void move( bool Direction )
+{
+  if( Direction )
+  {
+    moveForward();
+  }
+  else
+  {
+    moveBackwards();
+  }
+}
+
+void turn( bool Way )
+{
+  if( Way )
+  {
+    turnRight();
+  }
+  else
+  {
+    turnLeft();
+  }
+}
+
+void turnFull( bool Way )
+{
+  if( Way )
+  {
+    turnFullRight();
+  }
+  else
+  {
+    turnFullLeft();
+  }
+}
