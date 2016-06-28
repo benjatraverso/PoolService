31f6c4e1b57756e376119b763ae527b89600175f
benjatraverso <benjatraverso@gmail.com>
2016-06-28 20:43:09 -0300

    "release version"

--- Software/Boat/Boat_V09/Boat.h
+++ Software/Boat/Boat_V09/Boat.h
@@ -12,10 +12,10 @@ const byte MotorLB = 7;   // Control pin Backward for left motor
 const byte MotorRF = 12;  // Control pin Forward for right motor
 const byte MotorRB = 13;  // Control pin Backward for right motor
 
-const bool FORWARD              = 1;
-const bool BACKWARDS            = 0;
-const bool RIGHT                = 1;
-const bool LEFT                 = 0;
+const byte FORWARD              = 1;
+const byte BACKWARDS            = 0;
+const byte RIGHT                = 1;
+const byte LEFT                 = 0;
 
 const byte leftProximitySensor  = 2;   //interrupt input 0 reads left sensor (DP2 is interrupt 0)
 const byte rightProximitySensor = 3;   //interrupt input 1 reads right sensor (DP3 is interrupt 1)
@@ -27,14 +27,14 @@ const byte LOW_SPEED            = 75;
 const byte HIGH_SPEED           = 125;
 const int SECONDS               = 1000;
 const int STEPS_DELAY           = 3 * SECONDS;
-const int FULL_TURN_TIME      	= 10 * SECONDS;		// 10 seconds to pass from turning to 2 motors turnning
+const int FULL_TURN_TIME      	= 3 * SECONDS;		// 10 seconds to pass from turning to 2 motors turnning
 const int ERROR_TIME		        = 30 * SECONDS;   // seconds to determin we are stuck
 const int ERROR_DELAY           = 10 * SECONDS;
 const int POOLING_DELAY         = 1 * SECONDS;
 
-const int NEITHER     = 2;
-const int TURN_LEFT   = 0;
-const int TURN_RIGHT  = 1;
+const byte NEITHER     = 2;
+const byte TURN_LEFT   = 0;
+const byte TURN_RIGHT  = 1;
 
 //----------------------------------------------------------------------------
 //                            ENUMS
